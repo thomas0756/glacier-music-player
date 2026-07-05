79d5669400e3e5a0f795bc873091f1ef3cce3b6e
Thomas Hunt <170183176+thomas0756@users.noreply.github.com>
2026-07-05 11:40:42 +0100

    Add Song fields

--- song.cs
+++ song.cs
@@ -1,3 +1,10 @@
-class Song {
-    
+using System.Data.Common;
+using System.Net.NetworkInformation;
+
+class Song (UInt32 id, String title, String artist, UInt32 duration, String path){
+    UInt32 id = id;
+    String title = title;
+    String artist = artist;
+    UInt32 duration = duration;
+    String path = path;
 }
