0bc0315c11c59ac5170f10fb77f74de4e581b587
Thomas Hunt <170183176+thomas0756@users.noreply.github.com>
2026-07-05 13:26:35 +0100

    Create a GTK4 window

---
+++ gtkui.cs
@@ -0,0 +1,15 @@
+public static class GtkInterface
+{
+    public static int Init()
+    {
+        var application = Gtk.Application.New("com.thomas0756.glacier", Gio.ApplicationFlags.FlagsNone);
+        application.OnActivate += (sender, args) =>
+        {
+            var window = Gtk.ApplicationWindow.New((Gtk.Application)sender);
+            window.Title = "Glacier Music Player";
+            window.SetDefaultSize(300, 300);
+            window.Show();
+        };
+        return application.RunWithSynchronizationContext(null);
+    }
+}
