9baf637be8c213195e486d1b6d5ca3c4c99042d6
Thomas Hunt <170183176+thomas0756@users.noreply.github.com>
2026-07-07 22:08:54 +0100

    Add file format checks

--- library/metadatahandler.cs
+++ library/metadatahandler.cs
@@ -2,21 +2,45 @@ using TagLib;
 
 public static class MetadataHandler
 {
+    static readonly List<String> SupportedExtensions = [
+        ".mp3",
+        ".mp2",
+        ".mp1",
+        ".ogg",
+        ".wav",
+        ".mpa",
+        ".m2a",
+        ".m1a",
+        ".mpg",
+        ".mpeg",
+        ".mp3pro",
+        ".bwf",
+    ];
+
     public static Song? CreateSongFromFile(String path)
     {
         if (System.IO.File.Exists(path))
         {
-            TagLib.File song_file = TagLib.File.Create(path);
-            return new Song(
-                0, //Figure out an ID system once there's a database
-                song_file.Tag.Title,
-                song_file.Tag.FirstPerformer,
-                song_file.Properties.Duration.TotalSeconds,
-                path
-            );
+            if (SupportedExtensions.Contains(Path.GetExtension(path)))
+            {
+                TagLib.File song_file = TagLib.File.Create(path);
+                return new Song(
+                    0, //Figure out an ID system once there's a database
+                    song_file.Tag.Title,
+                    song_file.Tag.FirstPerformer,
+                    song_file.Properties.Duration.TotalSeconds,
+                    path
+                );
+            }
+            else
+            {
+                Console.WriteLine("Unsupported file format in '" + path + "'");
+                return null;
+            }
         }
         else
         {
+            Console.WriteLine("File '" + path + "' does not exist.");
             return null;
         }
     }
