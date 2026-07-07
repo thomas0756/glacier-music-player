73981db0c5d8e713a58f62dc7f88255dcd9d09d8
Thomas Hunt <170183176+thomas0756@users.noreply.github.com>
2026-07-07 21:51:33 +0100

    Add method to create Song from path

--- library/metadatahandler.cs
+++ library/metadatahandler.cs
@@ -1,4 +1,23 @@
+using TagLib;
+
 public static class MetadataHandler
 {
-    
+    public static Song? CreateSongFromFile(String path)
+    {
+        if (System.IO.File.Exists(path))
+        {
+            TagLib.File song_file = TagLib.File.Create(path);
+            return new Song(
+                0, //Figure out an ID system once there's a database
+                song_file.Tag.Title,
+                song_file.Tag.FirstPerformer,
+                song_file.Properties.Duration.TotalSeconds,
+                path
+            );
+        }
+        else
+        {
+            return null;
+        }
+    }
 }
